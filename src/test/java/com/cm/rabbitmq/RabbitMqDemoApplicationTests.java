a79250da3e2ef6afe9b0027e3fb70f5c25541116
陈萌 <2322051194@qq.com>
2022-04-01 21:17:47 +0800

    手动 ack

--- src/test/java/com/cm/rabbitmq/RabbitMqDemoApplicationTests.java
+++ src/test/java/com/cm/rabbitmq/RabbitMqDemoApplicationTests.java
@@ -1,30 +1,15 @@
 package com.cm.rabbitmq;
 
-import com.cm.rabbitmq.provider.ProviderService;
 import org.junit.jupiter.api.Test;
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class RabbitMqDemoApplicationTests {
 
-    @Autowired
-    private ProviderService providerService;
 
     @Test
     void contextLoads() {
-        providerService.sendMsg();
     }
 
-    @Test
-    void sendTopicMsg() {
-        providerService.sendTopicMsg();
-    }
-
-    @Test
-    void sendFanoutMsg() {
-        providerService.sendFanoutMsg();
-    }
 
 }
