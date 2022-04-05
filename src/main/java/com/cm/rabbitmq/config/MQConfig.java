618cb5e55485690f7c4d38b250e42e975af31672
陈萌 <2322051194@qq.com>
2022-04-05 17:13:04 +0800

    消息重入队列

--- src/main/java/com/cm/rabbitmq/config/MQConfig.java
+++ src/main/java/com/cm/rabbitmq/config/MQConfig.java
@@ -1,6 +1,9 @@
 package com.cm.rabbitmq.config;
 
 import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -163,6 +166,25 @@ public class MQConfig {
     public Binding warningQueueBindBackupExchange(@Qualifier(value = "warningQueue") Queue queue,
                                                   @Qualifier(value = "backupExchange") FanoutExchange exchange) {
         return BindingBuilder.bind(queue).to(exchange);
+    }
+
+    @Bean
+    public SimpleRabbitListenerContainerFactory rabbitListenerContainerFactory(ConnectionFactory connectionFactory) {
+        SimpleRabbitListenerContainerFactory factory = new SimpleRabbitListenerContainerFactory();
+        factory.setConnectionFactory(connectionFactory);
+        //设置并发
+        factory.setConcurrentConsumers(1);
+        //最大并发
+        factory.setMaxConcurrentConsumers(1);
+        //消息接收——手动确认
+        factory.setAcknowledgeMode(AcknowledgeMode.MANUAL);
+        //设置超时
+        factory.setReceiveTimeout(2000L);
+        //设置重试间隔
+        factory.setFailedDeclarationRetryInterval(3000L);
+        //监听自定义格式转换
+        factory.setMessageConverter(new Jackson2JsonMessageConverter());
+        return factory;
     }
 
 }
--- src/main/java/com/cm/rabbitmq/consumer/ConsumerService.java
+++ src/main/java/com/cm/rabbitmq/consumer/ConsumerService.java
@@ -8,6 +8,7 @@ import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 
 @Log4j2
 @Component
@@ -15,7 +16,7 @@ public class ConsumerService {
 
     @RabbitListener(queues = MQConfig.PRIORITY_QUEUE)
     public void process(Channel channel, Message message) throws IOException {
-        String testMessage = new String(message.getBody());
+        String testMessage = new String(message.getBody(), StandardCharsets.UTF_8);
         log.info("ConsumerService消费者收到消息:{}", testMessage);
         try {
             // 模拟执行任务
@@ -26,29 +27,35 @@ public class ConsumerService {
             // 确认收到消息，false只确认当前consumer一个消息收到，true确认所有consumer获得的消息
             channel.basicAck(message.getMessageProperties().getDeliveryTag(), false);
         } catch (Exception e) {
+            // basicNack可以批量拒绝多条消息，而basicReject一次只能拒绝一条消息。
             if (message.getMessageProperties().getRedelivered()) {
                 log.info("消息已重复处理失败,拒绝再次接收");
                 // 拒绝消息，requeue=false 表示不再重新入队，如果配置了死信队列则进入死信队列
-                channel.basicReject(message.getMessageProperties().getDeliveryTag(), false);
+                channel.basicReject(message.getMessageProperties().getDeliveryTag(), true);
             } else {
                 log.info("消息即将再次返回队列处理");
-                // requeue为是否重新回到队列，true重新入队
+                // multiple 是否批量. true：将一次性拒绝所有小于deliveryTag的消息
+                // equeue：被拒绝的是否重新入队列
                 channel.basicNack(message.getMessageProperties().getDeliveryTag(), false, true);
             }
         }
 
     }
 
+    /*
     @RabbitListener(queues = MQConfig.TOPIC_QUEUE)
     public void processTopic(String testMessage) {
         String fanoutQueue = MQConfig.TOPIC_QUEUE;
         log.info("ConsumerService.processTopic 队列：{}, 消息:{}", fanoutQueue, testMessage);
     }
+    */
+/*
 
     @RabbitListener(queues = MQConfig.FANOUT_QUEUE_1)
     public void processFanout(String testMessage) {
         String fanoutQueue = MQConfig.FANOUT_QUEUE_1;
         log.info("ConsumerService.processFanout 队列：{}, 消息:{}", fanoutQueue, testMessage);
     }
+*/
 
 }
