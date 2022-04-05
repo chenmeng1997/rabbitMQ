a311eb79f6b69b2bc8e95a18e6884f521208365d
陈萌 <2322051194@qq.com>
2022-04-05 17:14:30 +0800

    消息重入队列

--- src/main/java/com/cm/rabbitmq/config/CallBackConfig.java
+++ src/main/java/com/cm/rabbitmq/config/CallBackConfig.java
@@ -67,13 +67,4 @@ public class CallBackConfig implements RabbitTemplate.ConfirmCallback, RabbitTem
                 returnedMessage.getRoutingKey());
     }
 
-    @Bean
-    public SimpleRabbitListenerContainerFactory rabbitListenerContainerFactory(ConnectionFactory connectionFactory) {
-        SimpleRabbitListenerContainerFactory factory = new SimpleRabbitListenerContainerFactory();
-        factory.setAcknowledgeMode(AcknowledgeMode.MANUAL);
-        factory.setConnectionFactory(connectionFactory);
-        factory.setMessageConverter(new Jackson2JsonMessageConverter());
-        return factory;
-    }
-
 }
